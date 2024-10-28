ced9105fd2d5003c67607ead59325025cb419cfb
gabiroarnaud <gabiroarnaud@gmail.com>
2024-10-28 14:50:18 +0000

    Added user insertion for fetching workflows

--- src/app/(main)/(pages)/workflows/_actions/workflow-connections.tsx
+++ src/app/(main)/(pages)/workflows/_actions/workflow-connections.tsx
@@ -5,18 +5,25 @@ import { auth, currentUser } from '@clerk/nextjs'
 
 export const getGoogleListener = async () => {
   const { userId } = auth()
+  const user = await currentUser()
 
   if (userId) {
-    const listener = await db.user.findUnique({
-      where: {
+    // Check if the user exists
+    const listener = await db.user.upsert({
+      where: { clerkId: userId },
+      update: {},  // If the user exists, no update needed, so leave this empty
+      create: {    // If the user doesn't exist, create a new entry with provided details
         clerkId: userId,
+        name: user?.username,
+        email: user?.primaryEmailAddressId || '',
+        profileImage: user?.imageUrl,
       },
       select: {
         googleResourceId: true,
       },
-    })
+    });
 
-    if (listener) return listener
+    return listener;
   }
 }
 
